a5de76e2148517a74572c334d92aae2eabe80a15
Matthew Cote <mattcotecodeboxx@gmail.com>
2020-10-07 02:17:06 -0400

    Should have started this way from the beginning

---
+++ C-Sharp Commercial Controller/CommercialController/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CommercialController
+{
+    class Button {
+        // so about 15 mins ago, I deleted a complete modern commercial solution C#, by accident, on VS Studio Code, ironically I was trying to save my work on github, 
+        // but I wasn't thinking and I cloned an empty repository onto my project, instead of the other way around - zero backup, permanent delete as far as i can tell
+        // which is great news for me because tomorrow i can write it all again, right here, but first I will put this where it belongs, and back it up
+        
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
